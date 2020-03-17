ff0be0d3a0fe6e724e1cb7ca4e4bdc4baeb82b48
Jeff Derksen <jeff.derksen@pm.me>
2020-03-17 06:50:04 -0400

    lalt and lgui were swapped on jerksen keymap

--- keyboards/preonic/keymaps/jerksen/keymap.c
+++ keyboards/preonic/keymaps/jerksen/keymap.c
@@ -89,7 +89,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,       KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,     KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_DEL,  \
   CTL_T(KC_ESC),KC_A,    KC_R,    KC_S,    KC_T,    KC_D,     KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_BSPC, \
   LCTL(KC_LSFT),KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,  \
-  KC_LCTL,      KC_PSCR, KC_LGUI, KC_LALT, LOWER,   KC_LSFT, KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
+  KC_LCTL,      KC_PSCR, KC_LALT, KC_LGUI, LOWER,   KC_LSFT,  KC_SPC,  RAISE,   KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT  \
 ),
 
 /* Dvorak
